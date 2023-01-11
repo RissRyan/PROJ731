ee927c4b97ad15b74b163d6cb710fa742d5a78d0
Calvin <calvin.ngor@gmail.com>
2023-01-11 09:19:37 +0100

    update client.cpp

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,2 +1,23 @@
 #include "Client.h"
 
+
+
+Client::Client(std::string name)
+{
+	m_name += name;
+
+	std::cout << "Vous etes : " << m_name << std::endl;
+	sf::Socket::Status status = m_socket.connect("localhost", 53000);
+
+
+	if (status != sf::Socket::Done)
+	{
+		std::cout << "N'a pas pu se co au serveur\n";
+	}
+	else
+	{
+		std::cout << this->receiveMessage(&m_socket) << std::endl;
+	}
+
+	this->sendMessage(&m_socket, "ABCDEFGHIJ");
+}
